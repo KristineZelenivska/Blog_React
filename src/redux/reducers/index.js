b6ec3bc7369cfd16557a2378229716fb20cf4777
Haze <lindabuile@LVAGPLTP1255.dir.svc.accenture.com>
2019-05-31 16:34:22 +0300

    Created Redux folders

--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const initialState = {
-  articles: []
-};
-//returns the initial state without doing anything else
-function rootReducer(state = initialState, action) {
-  return state;
-}
-export default rootReducer;
